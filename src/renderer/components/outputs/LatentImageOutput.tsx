0050b63e71f494025c8c088160008f338de6929a
Alex Dodge <alexdodge@gmail.com>
2023-03-17 20:18:03 -0700

    ...

--- src/renderer/components/outputs/LatentImageOutput.tsx
+++ src/renderer/components/outputs/LatentImageOutput.tsx
@@ -3,7 +3,6 @@ import { Center, Flex, Spacer, Text } from '@chakra-ui/react';
 import { memo, useEffect } from 'react';
 import { useContext, useContextSelector } from 'use-context-selector';
 import { struct } from '../../../common/types/util';
-import { BackendContext } from '../../contexts/BackendContext';
 import { GlobalContext, GlobalVolatileContext } from '../../contexts/GlobalNodeState';
 import { TypeTags } from '../TypeTag';
 import { OutputProps } from './props';
@@ -13,63 +12,57 @@ interface LatentImageBroadcastData {
     height: number;
 }
 
-export const LatentImageOutput = memo(
-    ({ label, id, outputId, schemaId, useOutputData }: OutputProps) => {
-        const type = useContextSelector(GlobalVolatileContext, (c) =>
-            c.typeState.functions.get(id)?.outputs.get(outputId)
-        );
+export const LatentImageOutput = memo(({ label, id, outputId, useOutputData }: OutputProps) => {
+    const type = useContextSelector(GlobalVolatileContext, (c) =>
+        c.typeState.functions.get(id)?.outputs.get(outputId)
+    );
 
-        const { setManualOutputType } = useContext(GlobalContext);
+    const { setManualOutputType } = useContext(GlobalContext);
 
-        const outputIndex = useContextSelector(BackendContext, (c) =>
-            c.schemata.get(schemaId).outputs.findIndex((o) => o.id === outputId)
-        );
+    const { current } = useOutputData<LatentImageBroadcastData>(outputId);
+    useEffect(() => {
+        if (current) {
+            setManualOutputType(
+                id,
+                outputId,
+                struct('Image', {
+                    width: literal(current.width),
+                    height: literal(current.height),
+                })
+            );
+        } else {
+            setManualOutputType(id, outputId, undefined);
+        }
+    }, [id, outputId, current, setManualOutputType]);
 
-        const { current } = useOutputData<LatentImageBroadcastData>(outputId);
-        useEffect(() => {
-            if (current) {
-                setManualOutputType(
-                    id,
-                    outputId,
-                    struct('Image', {
-                        width: literal(current.width),
-                        height: literal(current.height),
-                    })
-                );
-            } else {
-                setManualOutputType(id, outputId, undefined);
-            }
-        }, [id, outputId, current, setManualOutputType]);
-
-        return (
-            <Flex
-                h="full"
-                minH="2rem"
-                verticalAlign="middle"
-                w="full"
-            >
-                <Spacer />
-                {type && (
-                    <Center
-                        h="2rem"
-                        verticalAlign="middle"
-                    >
-                        <TypeTags
-                            isOptional={false}
-                            type={type}
-                        />
-                    </Center>
-                )}
-                <Text
-                    h="full"
-                    lineHeight="2rem"
-                    marginInlineEnd="0.5rem"
-                    ml={1}
-                    textAlign="right"
+    return (
+        <Flex
+            h="full"
+            minH="2rem"
+            verticalAlign="middle"
+            w="full"
+        >
+            <Spacer />
+            {type && (
+                <Center
+                    h="2rem"
+                    verticalAlign="middle"
                 >
-                    {label}
-                </Text>
-            </Flex>
-        );
-    }
-);
+                    <TypeTags
+                        isOptional={false}
+                        type={type}
+                    />
+                </Center>
+            )}
+            <Text
+                h="full"
+                lineHeight="2rem"
+                marginInlineEnd="0.5rem"
+                ml={1}
+                textAlign="right"
+            >
+                {label}
+            </Text>
+        </Flex>
+    );
+});
