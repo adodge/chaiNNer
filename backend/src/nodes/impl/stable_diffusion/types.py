5cb55c341cd51b810235b78b10a2a2390520051b
Alex Dodge <alexdodge@gmail.com>
2023-03-29 03:23:58 -0700

    ...

--- backend/src/nodes/impl/stable_diffusion/types.py
+++ backend/src/nodes/impl/stable_diffusion/types.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import cv2
-import numpy as np
 from comfy.clip import CLIPModel
 from comfy.conditioning import Conditioning
 from comfy.latent_image import (
@@ -20,7 +18,6 @@ from comfy.stable_diffusion import (
     load_checkpoint,
 )
 from comfy.vae import VAEModel
-from PIL import Image
 
 __all__ = [
     "CLIPModel",
@@ -38,10 +35,3 @@ __all__ = [
     "RGBImage",
     "GreyscaleImage",
 ]
-
-
-def array_to_image(arr: np.ndarray) -> Image:
-    if arr.ndim == 3 and arr.shape[2] == 3:
-        arr = cv2.cvtColor(arr, cv2.COLOR_RGB2BGR)
-    arr = (np.clip(arr, 0, 1) * 255).round().astype("uint8")
-    return Image.fromarray(arr)
